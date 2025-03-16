68db1b395dd8b2f04b9b9794918804bf17d5c6b1
romann <imounga.r@gmail.com>
2025-03-16 23:42:47 +0100

    add introduction component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,26 @@
-import {Component, inject} from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
 import {UserService} from './core/services/user/user.service';
 import {DashboardComponent} from './features/dashboard/dashboard.component';
+import {IntroductionComponent} from './features/introduction/introduction.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   imports: [
-    DashboardComponent
+    DashboardComponent,
+    IntroductionComponent
   ],
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   private userService = inject(UserService);
-
   protected user = this.userService.getSingleUser();
   protected usersList = this.userService.getSeveralUsers(2);
+  showDashboard = false;
+
+  ngOnInit() {
+    setTimeout(() => {
+      this.showDashboard = true;
+    }, 5000);
+  }
 }
---
+++ src/app/features/introduction/introduction.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'si-introduction',
+  imports: [],
+  templateUrl: './introduction.component.html',
+  styleUrl: './introduction.component.scss',
+  standalone: true,
+})
+export class IntroductionComponent {
+
+}
