fa35dde25437275ec6a51e96f57a8a7cf67d63b3
romann <imounga.r@gmail.com>
2025-03-17 16:56:30 +0100

    little adjust on friends-modal

--- src/app/shared/components/modals/friends-modal/friends-modal.component.ts
+++ src/app/shared/components/modals/friends-modal/friends-modal.component.ts
@@ -1,9 +1,12 @@
 import {Component, input} from '@angular/core';
 import {UserModel} from '../../../../entities/user.model';
+import {NgIf} from '@angular/common';
 
 @Component({
   selector: 'si-friends-modal',
-  imports: [],
+  imports: [
+    NgIf
+  ],
   templateUrl: './friends-modal.component.html',
   styleUrl: './friends-modal.component.scss'
 })
